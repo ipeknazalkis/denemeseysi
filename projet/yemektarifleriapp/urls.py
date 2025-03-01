4b6b35a349e23b97027c629b6eea73785dc9ef56
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 17:06:18 +0300

    Update urls.py

--- projet/yemektarifleriapp/urls.py
+++ projet/yemektarifleriapp/urls.py
@@ -9,16 +9,15 @@ urlpatterns = [
 
 
 
+# myapp/urls.py
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from .views import girisn_view, sifre_unuttum
 
 urlpatterns = [
-    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('giris/', giris_view, name='giris'),
+    path('sifremi-unuttum/', sifre_unuttum, name='sifre_unuttum'),
+    # ... diğer URL'ler ...
 ]
-
 
 
 from django.urls import path
