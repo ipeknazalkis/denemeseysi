8b2a877c30752918204c425db30f2c8f2d62f0db
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 17:04:41 +0300

    Update views.py

--- projet/yemektarifleriapp/views.py
+++ projet/yemektarifleriapp/views.py
@@ -20,22 +20,53 @@ def kayit_hosgeldin_view(request, first_name, last_name):
 
 
 
+# myapp/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from django.contrib.auth import login
+from .forms import LoginForm
 
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+def giris_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
             login(request, user)
-            return redirect("dashboard")  # Kullanıcı giriş yaptıktan sonra yönlendirilecek sayfa
-        else:
-            messages.error(request, "Kullanıcı adı veya şifre hatalı!")
-    return render(request, "login.html")
+            return redirect('giris_hosgeldin', ad=user.ad, soyad=user.soyad
+    else:
+        form = LoginForm()
+    
+    return render(request, 'giris.html', {'form': form})
 
+
+# myapp/views.py
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth import get_user_model
+
+def sifre_unuttum(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        try:
+            user = get_user_model().objects.get(email=email)
+            current_site = get_current_site(request)
+            mail_subject = 'Şifre Sıfırlama'
+            message = render_to_string('sifre_sifirlama_emaili.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            return redirect('giris')
+        except get_user_model().DoesNotExist:
+            return render(request, 'sifre_unuttum.html', {'error': 'E-posta adresi bulunamadı'})
+    return render(request, 'sifre_unuttum.html')
 
 
 
