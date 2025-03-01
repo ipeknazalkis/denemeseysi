78b09be0847346e018cff95aa8d2b4b654e5bd53
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 18:57:31 +0300

    Update urls.py

--- projet/yemektarifleriapp/urls.py
+++ projet/yemektarifleriapp/urls.py
@@ -1,33 +1,15 @@
 # myapp/urls.py
 from django.urls import path
-from .views import kayit_view, kayit_hosgeldin_view
+from .views import (
+    kayit_view, kayit_hosgeldin_view, giris_view, sifre_unuttum, sifre_sifirlama
+)
 
 urlpatterns = [
-    path('kayit/', kayit_view, name='ksyit'),
+    path('kayit/', kayit_view, name='kayit'),
     path('merhaba/<str:ad>/<str:soyad>/', kayit_hosgeldin_view, name='kayit_hosgeldin'),
-]
 
-
-
-# myapp/urls.py
-from django.urls import path
-from .views import girisn_view, sifre_unuttum
-
-urlpatterns = [
     path('giris/', giris_view, name='giris'),
     path('sifremi-unuttum/', sifre_unuttum, name='sifre_unuttum'),
-    # ... diğer URL'ler ...
+
+    path('sifre-sifirla/<uidb64>/<token>/', sifre_sifirlama, name='sifre_sifirlama'),
 ]
-
-
-# myapp/urls.py
-from django.urls import path
-from .views import reset_password
-
-urlpatterns = [
-    # ... diğer URL'ler ...
-    path('sifre-sifirla/<uidb64>/<token>/', reset_password, name='sifre_sifirlama_dogrulaması'),
-]
-
-
-
