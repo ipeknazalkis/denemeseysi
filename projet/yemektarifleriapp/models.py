4f6383aa9b8b8da2c0f2127631fcd6dd867eed67
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 16:39:54 +0300

    Update models.py

--- projet/yemektarifleriapp/models.py
+++ projet/yemektarifleriapp/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
 class CustomUser(AbstractUser):
-    ad = models.CharField(max_length=50)
-    soyad = models.CharField(max_length=50)
+    ad = models.CharField(max_length=255)
+    soyad = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     dogum_tarihi = models.DateField(null=True, blank=True)
-    boy = models.FloatField(null=True, blank=True)  
-    kilo = models.FloatField(null=True, blank=True)  
+    boy = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True) 
+    kilo = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)  
 
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "kullanici.adi"
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ["email", "ad", "soyad"]
 
     def _str_(self):
-        return self.username
+        return self.kullanici.adi
+
+    def __str__(self):
+        return f"{self.ad} {self.soyad} ({self.kullanici_adi})"
