43c977b818084348b3741931b1c0015c8a40d6bb
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 17:55:37 +0300

    Update forms.py

--- projet/yemektarifleriapp/forms.py
+++ projet/yemektarifleriapp/forms.py
@@ -8,27 +8,27 @@ class CustomUserRegisterForm(UserCreationForm):
     ad = forms.CharField(max_length=255, required=True, label="Ad")
     soyad = forms.CharField(max_length=255, required=True, label="Soyad")
     email = forms.EmailField(required=True, label="E-Posta")
-    sifre = forms.CharField(widget=forms.PasswordInput)
-    sifre_dogrulama = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    pasword2 = forms.CharField(widget=forms.PasswordInput)
     dogum_tarihi = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     boy = forms.DecimalField(max_digits=5, decimal_places=2)
     kilo = forms.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         model = get_user_model()
-        fields = ["kullanici_adi", "ad", "soyad", "email", "sifre1", "sifre2", "dogum_tarihi", "boy", "kilo"]
+        fields = ["kullanici_adi", "ad", "soyad", "email", "password1", "password2", "dogum_tarihi", "boy", "kilo"]
 
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get('sifre')
-        confirm_password = cleaned_data.get('sifre_dogrulama')
+        sifre = cleaned_data.get('password1')
+        sifre_dogrulama = cleaned_data.get('password2')
 
         if sifre and sifre_dogrulama and sifre != sifre_dogrulama:
             raise forms.ValidationError('Parolalar eşleşmiyor')
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['sifre'])
+        user.set_password(self.cleaned_data['password1'])
         
         if commit:
             user.save()
@@ -42,16 +42,16 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
-    giris = forms.CharField(max_length=255)
+    login = forms.CharField(max_length=255)
     sifre = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
         cleaned_data = super().clean()
-        giris = cleaned_data.get('giris')
+        login = cleaned_data.get('login')
         sifre = cleaned_data.get('sifre')
 
         try:
-            user = get_user_model().objects.get(kullanici_adi=login)
+            user = get_user_model().objects.get(username=login)
         except get_user_model().DoesNotExist:
             try:
                 user = get_user_model().objects.get(email=login)
@@ -76,9 +76,12 @@ class ResetPasswordForm(forms.Form):
         yeni_sifre = cleaned_data.get('yeni_sifre')
         sifre_dogrulama = cleaned_data.get('sifre_dogrulama')
 
-        if yeni_sfire and sifre_dogrulama and yeni_sifre != sifre_dogrulama:
+        if yeni_sifre and sifre_dogrulama and yeni_sifre != sifre_dogrulama:
             raise forms.ValidationError('Parolalar eşleşmiyor')
 
-        cleaned_data['user'] = user
         return cleaned_data
+
+        if yeni_sifre and sifre_dogrulama and yeni_sifre != sifre_dogrulama:
+            raise forms.ValidationError('Parolalar eşleşmiyor')
+
 
