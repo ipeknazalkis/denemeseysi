02de8727e077f913340210587c6fe2874b61cbf6
ipeknazalkis <ipeknaz.alkis@ogr.gsu.edu.tr>
2025-03-01 20:32:35 +0300

    Update urls.py

--- projet/yemektarifleriapp/urls.py
+++ projet/yemektarifleriapp/urls.py
@@ -1,7 +1,7 @@
 # myapp/urls.py
 from django.urls import path
 from .views import (
-    kayit_view, kayit_hosgeldin_view, giris_view, giris_hosgeldin, sifre_unuttum, sifre_sifirlama
+    kayit_view, kayit_hosgeldin_view, giris_view, giris_hosgeldin_view, sifre_unuttum, sifre_sifirlama
 )
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path('merhaba/<str:first_name>/<str:last_name>/', kayit_hosgeldin_view, name='kayit_hosgeldin'),
 
     path('giris/', giris_view, name='giris'),
-    path('giris-hosgeldin/<str:ad>/<str:soyad>/', giris_hosgeldin_view, name='giris_hosgeldin'),
+    path('tekrardan_merhaba/<str:first_name>/<str:last_name>/', giris_hosgeldin_view, name='giris_hosgeldin'),
 
     path('sifremi-unuttum/', sifre_unuttum, name='sifre_unuttum'),
     path('sifre-sifirla/<uidb64>/<token>/', sifre_sifirlama, name='sifre_sifirlama'),
